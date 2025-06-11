f44ada766eb2285c661c0850ec9a5197d87e14ff
NickdeRooij <npb.derooij@student.avans.nl>
2025-06-11 13:57:26 +0200

    Changed lay-out selector page. Added css

--- Services/ITrashDataService.cs
+++ Services/ITrashDataService.cs
@@ -1,18 +1,28 @@
 using TrashBoard.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace TrashBoard.Services
 {
     public interface ITrashDataService
     {
         Task<IEnumerable<TrashDetection>> GetAllAsync();
+
         Task<IEnumerable<TrashDetection>> GetFilteredAsync(
-            DateTime? from = null, DateTime? to = null, List<string>? trashType = null);
+            DateTime? from,
+            DateTime? to,
+            List<string>? trashTypes,
+            bool? isHoliday);  // <-- nieuwe parameter
 
         Task<TrashDetection?> GetByIdAsync(int id);
+
         Task AddAsync(TrashDetection detection);
+
         Task UpdateWeatherInfoAsync(int id, float temperature, string condition, float humidity, float precipitation, DateTime date);
-        Task<TrashDetection> UpdateHolidayInfoForAsync(TrashDetection detection);
+
         Task<IEnumerable<string>> GetAvailableTrashTypesAsync();
 
+        Task<TrashDetection> UpdateHolidayInfoForAsync(TrashDetection detection);
     }
 }
