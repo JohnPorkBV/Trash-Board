44744d810652f641afff20745f84260aa8cb3190
S.k2004 <stormkroonen@hotmail.nl>
2025-06-17 18:13:05 +0200

    using grid to display data

--- Trash-Board/Services/TrashDataService.cs
+++ Trash-Board/Services/TrashDataService.cs
@@ -1,69 +1,41 @@
 using Microsoft.EntityFrameworkCore;
 using TrashBoard.Data;
 using TrashBoard.Models;
-using TrashBoard.Services;
 
 namespace TrashBoard.Services
 {
     public class TrashDataService : ITrashDataService
     {
-        private readonly TrashboardDbContext _context;
+        private readonly IDbContextFactory<TrashboardDbContext> _contextFactory;
         private readonly IHolidayService _holidayService;
         private readonly IBredaEventService _bredaEventService;
 
         public TrashDataService(
-            TrashboardDbContext context,
+            IDbContextFactory<TrashboardDbContext> contextFactory,
             IHolidayService holidayService,
             IBredaEventService bredaEventService)
         {
-            _context = context;
+            _contextFactory = contextFactory;
             _holidayService = holidayService;
             _bredaEventService = bredaEventService;
         }
 
-
-
         public async Task<IEnumerable<TrashDetection>> GetAllAsync()
         {
-            return await _context.TrashDetections.ToListAsync();
+            await using var context = _contextFactory.CreateDbContext();
+            return await context.TrashDetections.AsNoTracking().ToListAsync();
         }
 
-        //public async Task<IEnumerable<TrashDetection>> GetFilteredAsync(
-        //    DateTime? from,
-        //    DateTime? to,
-        //    List<string>? trashTypes,
-        //    bool? isHoliday,
-        //    bool? isBredaEvent)
-        //{
-        //    var query = _context.TrashDetections.AsQueryable();
-
-        //    if (from.HasValue)
-        //        query = query.Where(t => t.Timestamp >= from.Value);
-
-        //    if (to.HasValue)
-        //        query = query.Where(t => t.Timestamp <= to.Value);
-
-        //    if (trashTypes != null && trashTypes.Any())
-        //        query = query.Where(t => trashTypes.Contains(t.DetectedObject));
-
-        //    if (isHoliday.HasValue)
-        //        query = query.Where(t => t.IsHoliday == isHoliday.Value);
-
-        //    if (isBredaEvent.HasValue)
-        //        query = query.Where(t => t.IsBredaEvent == isBredaEvent.Value);
-
-        //    query = query.OrderByDescending(t => t.Timestamp);
-
-        //    return await query.ToListAsync();
-        //}
         public async Task<IEnumerable<TrashDetection>> GetFilteredAsync(
-    DateTime? from,
-    DateTime? to,
-    List<string>? trashTypes,
-    bool? isHoliday,
-    bool? isBredaEvent)
+            DateTime? from,
+            DateTime? to,
+            List<string>? trashTypes,
+            bool? isHoliday,
+            bool? isBredaEvent)
         {
-            var query = _context.TrashDetections
+            await using var context = _contextFactory.CreateDbContext();
+
+            var query = context.TrashDetections
                 .AsNoTracking()
                 .AsQueryable();
 
@@ -82,40 +54,29 @@ namespace TrashBoard.Services
             if (isBredaEvent.HasValue)
                 query = query.Where(t => t.IsBredaEvent == isBredaEvent.Value);
 
-            // Only select the fields needed by your Blazor page
             return await query
                 .OrderByDescending(t => t.Timestamp)
-                .Select(t => new TrashDetection
-                {
-                    Id = t.Id,
-                    Timestamp = t.Timestamp,
-                    DetectedObject = t.DetectedObject,
-                    ConfidenceScore = t.ConfidenceScore,
-                    Temp = t.Temp,
-                    Humidity = t.Humidity,
-                    Precipitation = t.Precipitation,
-                    Windforce = t.Windforce,
-                    IsHoliday = t.IsHoliday,
-                    HolidayName = t.HolidayName,
-                    IsBredaEvent = t.IsBredaEvent,
-                    BredaEventName = t.BredaEventName
-                })
                 .ToListAsync();
         }
 
+
         public async Task<TrashDetection?> GetByIdAsync(int id)
         {
-            return await _context.TrashDetections.FindAsync(id);
+            await using var context = _contextFactory.CreateDbContext();
+            return await context.TrashDetections.FindAsync(id);
         }
 
         public async Task AddAsync(TrashDetection detection)
         {
-            var holiday= await _holidayService.IsHolidayAsync(detection.Timestamp);
+            await using var context = _contextFactory.CreateDbContext();
+
+            var holiday = await _holidayService.IsHolidayAsync(detection.Timestamp);
             if (holiday != null)
             {
                 detection.IsHoliday = true;
                 detection.HolidayName = holiday.LocalName;
             }
+
             var bredaEvent = await _bredaEventService.HasBredaEventAsync(detection.Timestamp);
             if (bredaEvent != null)
             {
@@ -123,20 +84,26 @@ namespace TrashBoard.Services
                 detection.BredaEventName = bredaEvent.Name;
             }
 
-            _context.TrashDetections.Add(detection);
-            await _context.SaveChangesAsync();
+            context.TrashDetections.Add(detection);
+            await context.SaveChangesAsync();
         }
+
         public async Task<IEnumerable<string>> GetAvailableTrashTypesAsync()
         {
-            return await _context.TrashDetections
-                .Select(td => td.DetectedObject)
-                .Where(type => type != null && type != "")
+            await using var context = _contextFactory.CreateDbContext();
+
+            return await context.TrashDetections
+                .Where(t => !string.IsNullOrEmpty(t.DetectedObject))
+                .Select(t => t.DetectedObject!)
                 .Distinct()
-                .OrderBy(type => type)
+                .OrderBy(t => t)
                 .ToListAsync();
         }
+
         public async Task<TrashDetection> UpdateHolidayInfoForAsync(TrashDetection detection)
         {
+            await using var context = _contextFactory.CreateDbContext();
+
             var holiday = await _holidayService.IsHolidayAsync(detection.Timestamp);
 
             if (holiday != null)
@@ -150,13 +117,16 @@ namespace TrashBoard.Services
                 detection.HolidayName = null;
             }
 
-            _context.TrashDetections.Update(detection);
-            await _context.SaveChangesAsync();
+            context.TrashDetections.Update(detection);
+            await context.SaveChangesAsync();
 
             return detection;
         }
+
         public async Task<TrashDetection> UpdateBredaEventInfoForAsync(TrashDetection detection)
         {
+            await using var context = _contextFactory.CreateDbContext();
+
             var bredaEvent = await _bredaEventService.HasBredaEventAsync(detection.Timestamp);
 
             if (bredaEvent != null)
@@ -170,13 +140,10 @@ namespace TrashBoard.Services
                 detection.BredaEventName = null;
             }
 
-            _context.TrashDetections.Update(detection);
-            await _context.SaveChangesAsync();
+            context.TrashDetections.Update(detection);
+            await context.SaveChangesAsync();
 
             return detection;
         }
-
-
-
     }
 }
